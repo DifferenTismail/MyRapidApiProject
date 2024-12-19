08748d90a5c0fc06f3517dc69fed3b648632a125
İsmail Baran <ismailbaran04@gmail.com>
2024-12-19 21:45:31 +0300

    ilk kurulum yapıldı ve rapid api verilerini çekiceğimiz model oluşturuldu ardından controllera rapid apiden gelen kodu yapıştırdık ve index viewini oluşturduk

---
+++ MyRapidApiProject/Controllers/ImdbMovieController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRapidApiProject.Models;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace MyRapidApiProject.Controllers
+{
+    public class ImdbMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "3b6be351b7msh3c558acddcce9e9p1756ecjsn0e4f69419a09" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ImdbMovieViewModel>>(body);
+                return View(values.ToList());
+            }
+        }
+    }
+}
