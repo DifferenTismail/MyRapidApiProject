daa59e5a354899a49ce1f8efea5fef42ed4289b2
İsmail Baran <ismailbaran04@gmail.com>
2024-12-24 23:36:12 +0300

    doları tl ye dönüştürme apisine bağlantı yapıldı

---
+++ MyRapidApiProject/Controllers/ExchangeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace MyRapidApiProject.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert?from=USD&to=TRY&amount=1"),
+                Headers =
+    {
+        { "x-rapidapi-key", "3b6be351b7msh3c558acddcce9e9p1756ecjsn0e4f69419a09" },
+        { "x-rapidapi-host", "currency-conversion-and-exchange-rates.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var json = JObject.Parse(body);
+                ViewBag.v = json["result"];
+            }
+            return View();
+        }
+    }
+}
